f929d0ea5a3d88f28ca3e5c4a7993846da0815f3
whosm123 <2642677199@qq.com>
2026-02-26 11:15:11 +0800

    test

--- main.py
+++ main.py
@@ -292,18 +292,18 @@ class MyPlugin(Star):
             logger.error(f"要加好感度的用户 {user_id} 不在游戏名单中")
             message_chain = MessageChain().at(user_id, user_id).message(
                 f" [system] 好感度 + 1，当前好感度为 {self.love_levels[user_id]}")
-            self.context.send_message(event.unified_msg_origin, message_chain)
+            await self.context.send_message(event.unified_msg_origin, message_chain)
             return f"[system] 要加好感度的用户 {user_id} 不在游戏名单中！"
         elif self.love_levels[user_id] == 5:
             logger.error(f"用户 {user_id} 好感度已满（5），无法再增加")
             message_chain = MessageChain().at(user_id, user_id).message(
                 f" [system] 好感度 + 1，当前好感度为 {self.love_levels[user_id]}")
-            self.context.send_message(event.unified_msg_origin, message_chain)
+            await self.context.send_message(event.unified_msg_origin, message_chain)
             return f"用户 {user_id} 好感度已满（5），无法再增加"
         self.love_levels[user_id] += 1
         logger.info(f"用户 {user_id} 好感度 + 1，当前好感度为 {self.love_levels[user_id]}")
         message_chain = MessageChain().at(user_id,user_id).message(f" [system] 好感度 + 1，当前好感度为 {self.love_levels[user_id]}")
-        self.context.send_message(event.unified_msg_origin, message_chain)
+        await self.context.send_message(event.unified_msg_origin, message_chain)
         return f"用户 {user_id} 好感度 + 1，当前好感度为 {self.love_levels[user_id]}"
         # message = event.make_result()
         # message.at(user_id, user_id)
